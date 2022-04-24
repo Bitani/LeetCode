bac49ba16c91a9241670ce69802b30239efbdb85
Bitani <Bethanyaloz755@gmail.com>
2022-04-24 14:14:29 -0600

    Time: 1 ms (76.57%), Space: 43.3 MB (37.44%) - LeetHub

---
+++ 50-powx-n/50-powx-n.java
@@ -0,0 +1,6 @@
+class Solution {
+    public double myPow(double x, int n) {
+        //x is our number and n is the raised(power) number which means x raised to the power n.
+         return Math.pow(x, n);
+    }
+}
