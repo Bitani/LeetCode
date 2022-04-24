05ed0883f9737e06b4e765506b72bb8bda4f57b4
Bitani <Bethanyaloz755@gmail.com>
2022-04-24 15:14:44 -0600

    Time: 1 ms (90.84%), Space: 39.8 MB (77.19%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfFour(int num) {
+       if (num == 0)
+        {
+            return false;
+        }
+  else
+  {
+        return (Math.log10(num) / Math.log10(4)) % 1 == 0;
+    }
+}
+};
