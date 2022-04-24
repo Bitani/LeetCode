4aab766190c4a277a871e3f7cc444c6b8f8c10a1
Bitani <Bethanyaloz755@gmail.com>
2022-04-24 15:33:02 -0600

    Time: 0 ms (100.00%), Space: 6.2 MB (7.05%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,3 +1,11 @@
+/*
+The solution for power of four can be solved by the natural mathematical properties of logarithms.
+If n is a power of 4, then 4^x = n. This can be rewritten as log4 n = x, where x will be an integer if n is a power of 4.
+There is another propert so that log4 n can be rewritten as log n / log 4 which will produce a slight amount of floating point error, 
+but any value that is within a close margin (1e-10) while n is constrained to an int will be a correct.
+*/
+
+
 class Solution {
 public:
     bool isPowerOfFour(int num) {
