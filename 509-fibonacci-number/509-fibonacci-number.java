184e8b0af604200ff0f17029bad6d7a6495f7024
Bitani <Bethanyaloz755@gmail.com>
2022-04-24 13:19:45 -0600

    Time: 9 ms (28.93%), Space: 41.3 MB (19.31%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int fib(int num) {
+       if (num <= 1) 
+        {
+        return num;
+        }    
+   else
+     return fib(num - 1) + fib(num - 2);      
+    }
+}
