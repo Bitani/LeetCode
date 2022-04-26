ee0ffc9f7fc1e28b5f2325d87b3f5bc3714a570e
Bitani <Bethanyaloz755@gmail.com>
2022-04-26 14:46:57 -0600

    Time: 39 ms (34.19%), Space: 31.2 MB (48.42%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -3,22 +3,24 @@ public:
     int pivotIndex(vector<int>& nums)
     {
     int right_sum = 0;
+        
     for (auto n: nums) 
-    {
+     {
         right_sum = right_sum+n;
-    }
+     }
 
     int left_sum= 0;
+        
     for(int i=0; i<nums.size(); i++) 
-    {
+     {
         right_sum = right_sum-nums[i];
         
         if(left_sum == right_sum) 
-        {
+         {
             return i;
-        }
+         }
         left_sum = left_sum+nums[i];
-    }
+     }
 
     return -1;
     }
