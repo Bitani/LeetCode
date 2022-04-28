bbe497b6febd4574d3a28d652da40ec53bb99f0f
Bitani <Bethanyaloz755@gmail.com>
2022-04-28 11:36:31 -0600

    Time: 20 ms (48.88%), Space: 5.9 MB (34.12%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int num) {
+            string str  = to_string(num);//Changes the sigle number to a string;
+            int begin = 0;
+            int end = str.length() - 1;
+
+            while(begin < end)
+            {
+                if(str[begin++] != str[end--])
+                {
+                  return false;  
+                }  
+            }
+
+            return true;
+        }
+};
