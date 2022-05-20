671d30b119aca2eddc7f20ff2263331d8eb87254
Bitani <Bethanyaloz755@gmail.com>
2022-05-20 13:23:39 -0600

    Time: 4 ms (32.34%), Space: 6.3 MB (68.40%) - LeetHub

--- 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.cpp
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.cpp
@@ -1,17 +1,42 @@
 class Solution {
- public:
-  bool areNumbersAscending(string s) {
-    int prev = 0;
-    istringstream iss(s);
+public:
+    bool areNumbersAscending(string s) {
+        
+        int temp = -1;
+        for(int i = 0; i < s.length(); i++)
+        {
+            bool flag = false;
+            if (isdigit(s[i])) {
 
-    for (string token; iss >> token;)
-      if (isdigit(token[0])) {
-        const int num = stoi(token);
-        if (num <= prev)
-          return false;
-        prev = num;
-      }
+                // converting the first digit char to int
+                int num = s[i] - '0';
 
-    return true;
-  }
+                // checking if the digit has another digit after it
+                if (i < s.length() - 1 && isdigit(s[i + 1])) {
+
+                    // adding the digit to the digit before
+                    num = num * 10 + (s[i + 1] - '0');
+                    i++;
+
+                   //maintaining a flag bool so that we 
+                   //dont look for a third digit if this is false
+                    flag = true;
+                }
+
+               // checking the third digit, enters if flag is true
+                if (i < s.length() - 2 && isdigit(s[i + 1]) && flag) {
+                    num = num * 10 + (s[i + 2] - '0');
+                    i++;
+                }
+
+               // then the good old logic
+                if (num > temp)
+                    temp = num;
+                else
+                    return false;
+            }
+        }
+        
+        return true;
+    }
 };
