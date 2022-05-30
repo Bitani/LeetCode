7e8f67e299b07ed9c68eacae9ba7ff1d01a637b4
Bitani <Bethanyaloz755@gmail.com>
2022-05-30 09:10:48 -0600

    Time: 28 ms (54.73%), Space: 5.7 MB (89.37%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,15 +1,19 @@
-/*
-The solution for power of three can be solved by the natural mathematical properties of logarithms.
-
-If n is a power of 3, then 3^x = n. This can be rewritten as log3 n = x, where x will be an integer if n is a power of 3.
-There is another propert so that log3 n can be rewritten as log n / log 3 which will produce a slight amount of floating point error, 
-but any value that is within a close margin (1e-10) while n is constrained to an int will be a correct.
-
-*/
-
 class Solution {
-    public : bool isPowerOfThree(int num) {
-       double x = log(num) / log(3);
-        return abs(x - round(x)) < 1e-10;
+public:
+    bool isPowerOfThree(int n) {
+       if(n<=0)
+       {
+           return false;
+       }
+        if(n==1)
+        {
+            return true;
+        }
+        if(n%3==0)
+        {
+            n=n/3;
+            return isPowerOfThree(n); 
+        }
+        return false;
     }
 };
