9e38a179bd39643734704a1375c5cbd581c6da3a
Bitani <Bethanyaloz755@gmail.com>
2022-05-31 10:39:30 -0600

    Time: 24 ms (68.23%), Space: 5.9 MB (29.20%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-       if(n<=0)
-       {
-           return false;
-       }
-        if(n==1)
-        {
+        if (n == 1)
             return true;
-        }
-        if(n%3==0)
-        {
-            n=n/3;
-            return isPowerOfThree(n); 
-        }
-        return false;
+        if (n == 0)
+            return false;
+        
+        return !(n % 3) && isPowerOfThree(n / 3);
+
     }
 };
