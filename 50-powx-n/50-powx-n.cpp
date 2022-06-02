d5dc64c3c35e981c029094c87c0e0690d4e9f7fc
Bitani <Bethanyaloz755@gmail.com>
2022-06-02 10:40:24 -0600

    Time: 6 ms (11.30%), Space: 5.9 MB (80.49%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,8 +1,25 @@
 class Solution {
 public:
-//x is our number and n is the raised(power) number which means x raised to the power n.
     double myPow(double x, int n)
     {
-        return pow(x, n);
+        double ptr= 1;
+        if(n == 0)
+            return 1;
+        ptr = myPow(x , n/2);
+        if(n%2 == 0)
+        {
+         return ptr * ptr;   
+        }
+        else
+        {
+           if(n>0)  
+           {
+             return x*ptr*ptr;   
+           }
+          else
+          {
+            return (ptr*ptr)/x;
+          }
+        }
     }
 };
